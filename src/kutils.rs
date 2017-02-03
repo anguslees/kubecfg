06492dae0ef816d03ad49fee8c4f4a3b1d84958c
Angus Lees <gus@inodes.org>
2017-02-04 00:56:03 +1100

    Basic create/delete/update functionality
    Still a number of TODOs and unimplemented code branches, but this
    version is useful for basic operations.

--- src/kutils.rs
+++ src/kutils.rs
@@ -1,5 +1,9 @@
+use std::io::Read;
 use std::fmt;
 use json::JsonValue;
+use hyper::client::Response;
+
+use errors::*;
 
 #[derive(Clone,Copy,Debug,PartialEq)]
 pub struct K8sKind<'a> {
@@ -23,6 +27,37 @@ pub trait JsonValueExt {
     fn is_k8s_kind(&self, kind: K8sKind) -> bool;
     fn k8s_kind<'a>(&'a self) -> K8sKind<'a>;
     fn k8s_name(&self) -> &JsonValue;
+    fn k8s_namespace(&self) -> &JsonValue;
+
+    /// Type and name
+    fn k8s_tname(&self) -> String {
+        let kind = self.k8s_kind();
+        format!("{}/{}", kind.kind.to_lowercase(), self.k8s_name())
+    }
+
+    fn k8s_api_path(&self) -> String {
+        if self.is_k8s_kind(V1_NAMESPACE) {
+            String::from("/api/v1/namespaces")
+        } else {
+            let kind = self.k8s_kind();
+            // TODO: there is some autodiscovery I'm meant to be doing here
+            if kind.api_version.starts_with("extension") {
+                format!("/apis/{}/namespaces/{}/{}s",
+                        kind.api_version.to_lowercase(),
+                        self.k8s_namespace(),
+                        kind.kind.to_lowercase())
+            } else {
+                format!("/api/{}/namespaces/{}/{}s",
+                        kind.api_version.to_lowercase(),
+                        self.k8s_namespace(),
+                        kind.kind.to_lowercase())
+            }
+        }
+    }
+
+    fn k8s_api_path_named(&self) -> String {
+        format!("{}/{}", self.k8s_api_path(), self.k8s_name())
+    }
 }
 impl JsonValueExt for JsonValue {
     fn is_k8s_kind(&self, kind: K8sKind) -> bool {
@@ -36,9 +71,9 @@ impl JsonValueExt for JsonValue {
         }
     }
 
-    fn k8s_name(&self) -> &JsonValue {
-        &self["metadata"]["name"]
-    }
+    fn k8s_name(&self) -> &JsonValue { &self["metadata"]["name"] }
+
+    fn k8s_namespace(&self) -> &JsonValue { &self["metadata"]["namespace"] }
 }
 
 fn is_potential_pod_dependency(v: &JsonValue) -> bool {
@@ -49,6 +84,7 @@ fn is_potential_pod_dependency(v: &JsonValue) -> bool {
 }
 
 pub enum Operation {
+    Alpha, // "alphabetically" sort, presumably for display
     Create,
     Update,
     Delete,
@@ -66,9 +102,10 @@ pub fn sort_for(op: Operation, v: &JsonValue) -> Vec<&JsonValue> {
     }).collect();
 
     let v = match op {
-        Operation::Update | Operation::Delete => v,
+        Operation::Delete => v,
+        // Sort PV/PVC last?
 
-        Operation::Create => {
+        Operation::Create | Operation::Update => {
             enum Rank {
                 First,
                 Early,
@@ -82,7 +119,57 @@ pub fn sort_for(op: Operation, v: &JsonValue) -> Vec<&JsonValue> {
             });
             v
         },
+
+        Operation::Alpha => {
+            v.sort_by_key(|item| {
+                item["metadata"]["name"].as_str()
+            });
+            v
+        }
     };
 
     v
+}
+
+#[derive(Default,Debug)]
+pub struct DeleteOptions {
+    pub orphan_dependents: bool,
+    pub grace_period_seconds: Option<u32>,
+    pub preconditions: Vec<String>,
+}
+
+impl From<DeleteOptions> for JsonValue {
+    fn from(o: DeleteOptions) -> Self {
+        let mut res = object!{
+            "apiVersion" => "v1",
+            "kind" => "DeleteOptions",
+            "orphanDependents" => o.orphan_dependents,
+            "preconditions" => o.preconditions
+        };
+
+        if let Some(n) = o.grace_period_seconds {
+            res["gracePeriodSeconds"] = n.into();
+        }
+
+        res
+    }
+}
+
+pub fn kube_result(mut resp: Response) -> Result<JsonValue> {
+    let json = {
+        let mut body = String::new();
+        resp.read_to_string(&mut body)?;
+        if body.is_empty() {
+            JsonValue::Null
+        } else {
+            ::json::parse(&body)
+                .chain_err(|| "Unable to parse JSON response")?
+        }
+    };
+
+    if resp.status.is_success() {
+        Ok(json)
+    } else {
+        Err(ErrorKind::Kubernetes(json).into())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,8 @@ extern crate yaml_rust;
 extern crate json;
 #[macro_use]
 extern crate error_chain;
+extern crate hyper;
+extern crate url;
 
 mod errors {
     error_chain! {
@@ -21,6 +23,16 @@ mod errors {
                 description("Unknown output format")
                 display("Unknown output format: '{}'", v)
             }
+
+            Kubernetes(v: ::json::JsonValue) {
+                description("Error from Kubernetes server")
+                display("Error from Kubernetes: {}",
+                        if v["message"].is_empty() {
+                            &v["reason"]
+                        } else {
+                            &v["message"]
+                        })
+            }
         }
     }
 }
@@ -30,12 +42,16 @@ mod kutils;
 
 use clap::{Arg, App, SubCommand, AppSettings, Shell, ArgGroup};
 use jsonnet::{jsonnet_version,JsonnetVm};
+use url::Url;
+use hyper::Client;
+use hyper::header::{ContentType,Accept};
 use std::ffi::{OsStr,OsString};
+use std::io::{self,Write};
 use std::env;
 
 use errors::*;
 use emitters::OutputFormat;
-use kutils::JsonValueExt;
+use kutils::{JsonValueExt,kube_result};
 
 const JPATH_ENVVAR: &'static str = "KUBECFG_JPATH";
 
@@ -63,13 +79,13 @@ fn build_cli<'a>(version: &'a str) -> App<'a, 'a> {
         .arg(Arg::with_name("server")
              .short("s")
              .long("server")
-             .default_value("localhost:8001")
-             .value_name("HOST:PORT")
-             .help("The address and port of the Kubernetes API server"))
+             .default_value("http://localhost:8001/")
+             .value_name("URL")
+             .help("The URL of the Kubernetes API server"))
         .arg(Arg::with_name("namespace")
              .short("n")
              .long("namespace")
-             .value_name("NS")
+             .value_name("STR")
              .help("The namespace for this request"))
         .subcommand(SubCommand::with_name("completions")
                     .about("Generate shell completions")
@@ -104,6 +120,19 @@ fn build_cli<'a>(version: &'a str) -> App<'a, 'a> {
                     .group(ArgGroup::with_name("value")
                            .args(&["exec", "file"])
                            .required(true)))
+        .subcommand(SubCommand::with_name("diff")
+                    .about("Show differences between local files and running service")
+                    .arg(Arg::with_name("jpath")
+                         .short("J")
+                         .long("jpath")
+                         .value_name("DIR")
+                         .multiple(true)
+                         .help("Additional jsonnet library search path"))
+                    .arg(Arg::with_name("file")
+                         .short("f")
+                         .long("file")
+                         .value_name("FILE")
+                         .help("Input file")))
         .subcommand(SubCommand::with_name("create")
                     .about("Create resources only if they do not exist")
                     .arg(Arg::with_name("jpath")
@@ -119,18 +148,61 @@ fn build_cli<'a>(version: &'a str) -> App<'a, 'a> {
                          .required(true)
                          .help("Input file")))
         .subcommand(SubCommand::with_name("delete")
-                    .about("Delete named resources"))
+                    .about("Delete named resources")
+                    .arg(Arg::with_name("grace_period")
+                         .long("grace-period")
+                         .value_name("SECS")
+                         .help("Period of time in seconds given to the resource to terminate gracefully."))
+                    .arg(Arg::with_name("jpath")
+                         .short("J")
+                         .long("jpath")
+                         .value_name("DIR")
+                         .multiple(true)
+                         .help("Additional jsonnet library search path"))
+                    .arg(Arg::with_name("file")
+                         .short("f")
+                         .long("file")
+                         .value_name("FILE")
+                         .required(true)
+                         .help("Input file")))
         .subcommand(SubCommand::with_name("update")
                     .about("Update existing resources")
                     .arg(Arg::with_name("create")
                          .long("create")
-                         .help("Create missing resources")))
+                         .help("Create missing resources"))
+                    .arg(Arg::with_name("wait")
+                         .long("wait")
+                         .help("Block until update has completed"))
+                    .arg(Arg::with_name("jpath")
+                         .short("J")
+                         .long("jpath")
+                         .value_name("DIR")
+                         .multiple(true)
+                         .help("Additional jsonnet library search path"))
+                    .arg(Arg::with_name("file")
+                         .short("f")
+                         .long("file")
+                         .value_name("FILE")
+                         .required(true)
+                         .help("Input file")))
         .subcommand(SubCommand::with_name("check")
                     .about("Validate file against jsonschema")
+                    .arg(Arg::with_name("jpath")
+                         .short("J")
+                         .long("jpath")
+                         .value_name("DIR")
+                         .multiple(true)
+                         .help("Additional jsonnet library search path"))
                     .arg(Arg::with_name("file")
                          .value_name("FILE")
                          .required(true)
                          .help("Input file")))
+}
+
+struct Context {
+    vm: JsonnetVm,
+    server_url: Url,
+    client: Client,
 }
 
 fn init_vm_options<'a>(vm: &mut JsonnetVm, env_path: Option<OsString>, matches: &::clap::ArgMatches<'a>) {
@@ -169,8 +241,7 @@ fn eval_file_or_snippet<'a, 'b>(vm: &'b mut JsonnetVm, matches: &::clap::ArgMatc
 
 fn main() {
     if let Err(ref e) = main_() {
-        use ::std::io::Write;
-        let stderr = &mut ::std::io::stderr();
+        let stderr = &mut io::stderr();
         let errmsg = "Error writing to stderr";
 
         writeln!(stderr, "error: {}", e).expect(errmsg);
@@ -188,6 +259,74 @@ fn main() {
     }
 }
 
+//  frob:
+// +  xyzzy
+//    foo:
+// +    bar:
+// +     subbar
+// -    baz
+
+#[derive(Debug,PartialEq,Clone,Copy)]
+enum DiffDir {
+    AOnly,
+    BOnly,
+    Both,
+}
+impl DiffDir {
+    fn unidiff_prefix(&self) -> &'static str {
+        match *self {
+            DiffDir::AOnly => "-",
+            DiffDir::BOnly => "+",
+            DiffDir::Both  => " ",
+        }
+    }
+}
+
+struct Diff<'a> {
+    dir: DiffDir,
+    depth: u16,
+    value: &'a json::JsonValue,
+}
+fn diff_walk<'a>(depth: u16, a: Option<&'a json::JsonValue>, b: Option<&'a json::JsonValue>) -> Vec<Diff<'a>> {
+    use std::collections::BTreeSet;
+    match (a, b) {
+        (None, None) => vec![],
+        (Some(a), None) =>
+            vec![Diff{dir: DiffDir::AOnly, depth: depth, value: a}],
+        (None, Some(b)) =>
+            vec![Diff{dir: DiffDir::BOnly, depth: depth, value: b}],
+        (Some(a), Some(b)) => {
+            if a.is_array() && b.is_array() {
+                unimplemented!()
+            } else if a.is_object() && b.is_object() {
+                let keys: BTreeSet<_> = a.entries()
+                    .chain(b.entries())
+                    .map(|v| v.0)
+                    .collect();
+                let mut keys: Vec<_> = keys.into_iter().collect();
+                keys.sort();
+
+                let mut diffs = vec![];
+                for k in keys {
+                    if !a.has_key(k) {
+                        diffs.push(Diff{dir: DiffDir::BOnly, depth: depth+1, value: &b[k]});
+                    } else if !b.has_key(k) {
+                        diffs.push(Diff{dir: DiffDir::AOnly, depth: depth+1, value: &a[k]});
+                    }
+                }
+                diffs
+            } else {
+                if a == b {
+                    vec![]
+                } else {
+                    vec![Diff{dir: DiffDir::AOnly, depth: depth, value: a},
+                         Diff{dir: DiffDir::BOnly, depth: depth, value: b}]
+                }
+            }
+        },
+    }
+}
+
 fn main_() -> Result<()> {
     env_logger::init()
         .chain_err(|| "Error initialising logging")?;
@@ -196,6 +335,11 @@ fn main_() -> Result<()> {
 
     let version = format!("{} (jsonnet {})", crate_version!(), jsonnet_version());
     let matches = build_cli(&version).get_matches();
+
+    let server_url = Url::parse(matches.value_of("server").unwrap())
+        .chain_err(|| "Invalid --server URL")?;
+
+    let client = Client::new();
 
     if let Some(ref matches) = matches.subcommand_matches("completions") {
         let shell = value_t!(matches, "shell", Shell)
@@ -228,19 +372,189 @@ fn main_() -> Result<()> {
         let objects = kutils::sort_for(kutils::Operation::Create, &parsed);
 
         for o in objects {
-            println!("name: {} {}", o.k8s_kind(), o.k8s_name());
-            println!("object: {:?}", o);
+            let url_path = o.k8s_api_path();
+            let url = server_url
+                .join(&url_path)
+                .chain_err(|| "Unable to create URL")?;
+            let body = o.dump();
+
+            // TODO: support --record?
+
+            info!("=> POST {}", url);
+            let req = client.post(url)
+                .header(ContentType::json())
+                .header(Accept::json())
+                .body(&body);
+
+            let resp = req.send()
+                .chain_err(|| "Error sending request")?;
+            info!("<= {}", resp.status);
+
+            kube_result(resp)?;
         }
 
-        // next step: hyper
+    } else if let Some(ref matches) = matches.subcommand_matches("delete") {
+        let mut vm = JsonnetVm::new();
+        init_vm_options(&mut vm, jpath_env, matches);
 
-        unimplemented!();
-    } else if let Some(ref _matches) = matches.subcommand_matches("delete") {
-        unimplemented!();
-    } else if let Some(ref _matches) = matches.subcommand_matches("update") {
-        unimplemented!();
-    } else if let Some(ref _matches) = matches.subcommand_matches("check") {
-        unimplemented!();
+        let filename = matches.value_of_os("file").unwrap();
+        let json = vm.evaluate_file(filename)
+            .map_err(|e| e.as_str().to_owned())?;
+
+        let parsed = json::parse(&json)
+            .chain_err(|| "Unable to parse jsonnet output")?;
+
+        let objects = kutils::sort_for(kutils::Operation::Delete, &parsed);
+
+        let options: json::JsonValue = {
+            let mut o = kutils::DeleteOptions::default();
+
+            if let Some(n) = matches.value_of("grace_period") {
+                let v = n.parse()
+                    .chain_err(|| "Invalid --grace-period")?;
+                o.grace_period_seconds = Some(v);
+            }
+
+            o.into()
+        };
+        let body = options.dump();
+
+        for o in objects {
+            let url_path = o.k8s_api_path_named();
+            let url = server_url
+                .join(&url_path)
+                .chain_err(|| "Unable to create URL")?;
+
+            info!("DELETE {}", url);
+            let req = client.delete(url)
+                .header(ContentType::json())
+                .header(Accept::json())
+                .body(&body);
+
+            let resp = req.send()
+                .chain_err(|| "Error sending request")?;
+
+            kube_result(resp)?;
+        }
+
+    } else if let Some(ref matches) = matches.subcommand_matches("update") {
+        let creat = matches.is_present("create");
+
+        let mut vm = JsonnetVm::new();
+        init_vm_options(&mut vm, jpath_env, matches);
+
+        let filename = matches.value_of_os("file").unwrap();
+        let json = vm.evaluate_file(filename)
+            .map_err(|e| e.as_str().to_owned())?;
+
+        let parsed = json::parse(&json)
+            .chain_err(|| "Unable to parse jsonnet output")?;
+
+        let objects = kutils::sort_for(kutils::Operation::Update, &parsed);
+
+        for o in objects {
+            let url_path = o.k8s_api_path_named();
+            let url = server_url
+                .join(&url_path)
+                .chain_err(|| "Unable to create URL")?;
+
+            // TODO: set kubernetes.io/change-cause ?
+            let body = o.dump();
+
+            info!("=> PATCH {}", url);
+            let req = client.patch(url)
+                .header(ContentType("application/merge-patch+json".parse().unwrap()))
+                .header(Accept::json())
+                .body(&body);
+
+            let mut resp = req.send()
+                .chain_err(|| "Error sending request")?;
+            info!("<= {}", resp.status);
+
+            if creat && resp.status == hyper::NotFound {
+                // Not found => create
+                info!("Creating {}", o.k8s_tname());
+                let url_path = o.k8s_api_path();
+                let url = server_url
+                    .join(&url_path)
+                    .chain_err(|| "Unable to create URL")?;
+
+                info!("=> POST {}", url);
+                let req = client.post(url)
+                    .header(ContentType::json())
+                    .header(Accept::json())
+                    .body(&body);
+
+                resp = req.send()
+                    .chain_err(|| "Error sending request")?;
+                info!("<= {}", resp.status);
+            }
+
+            kube_result(resp)?;
+
+            // TODO: Implement --wait.
+            // TODO: (Optionally) Show diff between orig and server response
+        }
+    } else if let Some(ref matches) = matches.subcommand_matches("check") {
+        let mut vm = JsonnetVm::new();
+        init_vm_options(&mut vm, jpath_env, matches);
+
+        let filename = matches.value_of_os("file").unwrap();
+        let json = vm.evaluate_file(filename)
+            .map_err(|e| e.as_str().to_owned())?;
+
+        let parsed = json::parse(&json)
+            .chain_err(|| "Unable to parse jsonnet output")?;
+
+        // TODO: jsonschema validation
+        let _ = parsed;
+        warn!("jsonschema validation not yet implemented");
+    } else if let Some(ref matches) = matches.subcommand_matches("diff") {
+        let mut vm = JsonnetVm::new();
+        init_vm_options(&mut vm, jpath_env, matches);
+
+        let filename = matches.value_of_os("file").unwrap();
+        let json = vm.evaluate_file(filename)
+            .map_err(|e| e.as_str().to_owned())?;
+
+        let parsed = json::parse(&json)
+            .chain_err(|| "Unable to parse jsonnet output")?;
+
+        let objects = kutils::sort_for(kutils::Operation::Alpha, &parsed);
+
+        // TODO: optionally find everything else already in the namespace
+
+        for o in objects {
+            let url_path = o.k8s_api_path_named();
+            let url = server_url
+                .join(&url_path)
+                .chain_err(|| "Unable to create URL")?;
+
+            info!("=> GET {}", url);
+            let req = client.get(url)
+                .header(Accept::json());
+
+            let resp = req.send()
+                .chain_err(|| "Error sending request")?;
+            info!("<= {}", resp.status);
+
+            let existing = if resp.status == hyper::NotFound {
+                None
+            } else {
+                Some(kube_result(resp)?)
+            };
+
+            let diffs = diff_walk(0, existing.as_ref(), Some(o));
+            for diff in diffs {
+                let indent: String = ::std::iter::repeat("  ")
+                    .take(diff.depth as usize)
+                    .collect();
+                println!("{}{}{}",
+                         diff.dir.unidiff_prefix(),
+                         indent,
+                         diff.value);
+            }
+        }
     } else {
         unreachable!();
     }
